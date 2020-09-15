9f1f7b96ba06f5a6bcb7623b43420ceb885b99ce
JooEHyeon <pjh980918@naver.com>
2020-09-15 12:08:12 +0900

    menu_xml_done

---
+++ latend_menu/app/src/main/java/com/example/latend_menu/Help.kt
@@ -0,0 +1,11 @@
+package com.example.latend_menu
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Help : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_help)
+    }
+}
---
+++ latend_menu/app/src/main/java/com/example/latend_menu/UpdateInfo.kt
@@ -0,0 +1,11 @@
+package com.example.latend_menu
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class UpdateInfo : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_update_info)
+    }
+}
