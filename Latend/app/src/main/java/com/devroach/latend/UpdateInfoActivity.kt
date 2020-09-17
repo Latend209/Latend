9d8459a6407988b055392f0c190fd67d69ee68ab
dionidip <paulus0617@gmail.com>
2020-09-17 22:03:14 +0900

    splash and login

---
+++ Latend/app/src/main/java/com/devroach/latend/UpdateInfoActivity.kt
@@ -0,0 +1,11 @@
+package com.devroach.latend
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class UpdateInfoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_update_info)
+    }
+}
